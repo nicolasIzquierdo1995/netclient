ab04053c7f7214becc864b0943fcaa52cdedc729
nicoizquierdo95 <nicoizquierdo95@gmail.com>
2019-10-25 00:37:25 -0300

    First commit

---
+++ Todo.Mvc.Ui/Controllers/TodoApiController.cs
@@ -0,0 +1,101 @@
+﻿//Copyright 2017 (c) SmartIT. All rights reserved.
+//By John Kocer
+// This file is for Swagger test, this application does not use this file
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using SmartIT.Employee.MockDB;
+using Todo.Mvc.Ui;
+
+namespace TodoAngular.Ui.Controllers
+{
+  [Produces("application/json")]
+  [Route("api/Todo")]
+  public class TodoApiController : Controller
+  {
+    TodoRepository _todoRepository = new TodoRepository();
+    private const string URL = "http://localhost:8080/webir/service/sendToken";
+
+    [Route("~/api/LoginUser")]
+    [HttpPost]
+    public HttpResponseMessage LoginUser([FromBody]Credential credentials)
+    {
+        // si mis credenciales estan en la clase estática
+        if (Users.Login(credentials.User, credentials.Password))
+        {
+            // obtengo un token
+            var token = Users.GetToken();
+
+            try
+            {
+                // se lo envio a java
+                SendTokenToServer(token);
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = new StringContent(token);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+                return response;
+            }
+            catch (Exception ex)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                response.Content = new StringContent(ex.Message);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+                return response;
+            }
+        }
+        else
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            response.Content = new StringContent("Todo roto");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+            return response;
+        }
+    }
+
+        [Route("~/api/RegisterUser")]
+        [HttpPost]
+        public HttpResponseMessage Register([FromBody]Credential credentials)
+        {
+            // agrego el usuario a la clase estática
+            Users.AddUser(credentials.User, credentials.Password);
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent("Todo joya");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+            return response;
+        }
+
+        private void SendTokenToServer(string token)
+        {
+            // creo el cliente
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8080");
+            string urlParameters = "?token=" + token;
+
+            // no estoy seguro si se hace así el post
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("token", token)
+            });
+
+            // List data response.
+            HttpResponseMessage response = client.PostAsync("/webir/service/sendToken", content).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("se rompió todo papu");
+            }
+
+            client.Dispose();
+
+        }
+
+    }
+
+    public class Credential
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Todo.Mvc.Ui/Users.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Mvc.Ui
+{
+        public static class Users
+        {
+            public static List<User> UsersList = new List<User>();
+
+            public static void AddUser(string name, string password)
+            {
+                UsersList.Add(new User()
+                {
+                    Name = name,
+                    Password = password
+                });
+            }
+
+            public static bool Login(string name, string encodedPassword)
+            {
+                return UsersList.Any(x => x.Name == name && x.Password == encodedPassword);
+            }
+
+            public static string GetToken()
+            {
+                return ComputeSha256Hash("pasa papu");
+            }
+
+            static string ComputeSha256Hash(string rawData)
+            {
+                // Create a SHA256   
+                using (SHA256 sha256Hash = SHA256.Create())
+                {
+                    // ComputeHash - returns byte array  
+                    byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                    // Convert byte array to a string   
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        builder.Append(bytes[i].ToString("x2"));
+                    }
+                    return builder.ToString();
+                }
+            }
+        }
+}
